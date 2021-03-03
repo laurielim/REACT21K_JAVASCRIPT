8a0288004790d7214a87eb4552a43e6318c185e1
laurielim <laurie.limsam@gmail.com>
2021-03-03 17:06:59 +0200

    upload solution for day 7 practice exercise

--- basic-js/day-10/script.js
+++ basic-js/day-10/script.js
@@ -7,22 +7,61 @@
  * 5.When user clicks on each of the pokemon name, make another API call to retrieve that pokemon information and show it on the screen, use the front_default value under sprites of each pokemon as the display image
  */
 
-let pokemons;
 // Make API call
-fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000")
+fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151")
   // Parse JSON
   .then((response) => response.json())
-  // Assign array of pokemonts to a variable
-  .then((data) => (pokemons = data));
+  .then((data) => {
+    // Assign array of pokemonts to a variable
+    let pokemons = data.results;
+    // Iterate through each Pokemon to display them
+    pokemons.forEach((pokemon) => addPokemonName(pokemon.name));
+
+    // Add event listener to each pokemon
+    document.querySelectorAll("a").forEach((anchor) => {
+      anchor.addEventListener("click", function () {
+        // When user clicks on pokemon name, make another API call to retrieve that pokemon information
+        fetch(`https://pokeapi.co/api/v2/pokemon/${this.innerHTML}`)
+          // Parse JSON
+          .then((response) => response.json())
+          .then((data) => {
+            // Display pokemon sprite on screen
+            showPokemon(this.innerHTML, data.sprites.front_default);
+          });
+      });
+    });
+  });
 
 /**
  * Given a name, it will add it to poke-list as an anchor tag.
  * @param {String} name
  */
 const addPokemonName = (name) => {
-  // Create new anchor tag
-  let anchor = document.createElement("a");
-  anchor.innerHTML = name;
-  anchor.href = `#${name}`;
-  document.getElementById("poke-list").appendChild(anchor);
+  // Create new div for pokemon
+  let pokemon = document.createElement("div");
+  // Add an anchor tag inside div with pokemon name
+  pokemon.innerHTML = `<a href = "#${name}">${name}</a>`;
+  // Append anchor to poke-list section
+  document.getElementById("poke-list").appendChild(pokemon);
+};
+
+/**
+ * Given a name and a url to the pokemon sprite, displays the the pokemon sprite
+ * @param {String} name pokemon name
+ * @param {String} link url to pokemon sprite
+ */
+const showPokemon = (name, link) => {
+  // Check if there is an existing sprite
+  if (document.querySelectorAll("#poke-info > img").length > 0) {
+    // Remove any existing sprite
+    document.getElementById("poke-info").innerHTML = "";
+  }
+  // Create a new "img" element
+  let image = document.createElement("img");
+  // Add given link as the image source
+  image.src = link;
+  // Add alt text, including pokemon name
+  image.alt = `Default front sprite of ${name}`;
+  // Append image to poke-info section
+  document.getElementById("poke-info").appendChild(image);
 };
