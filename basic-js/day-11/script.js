1a6ec363e6d3f929d045928384d147f59a4ada7a
laurielim <laurie.limsam@gmail.com>
2021-03-10 11:02:50 +0200

    add form to get city name

---
+++ basic-js/day-11/script.js
@@ -0,0 +1,3 @@
+/**
+ * In this lesson you are expected to study the technical documentation of the the OpenWeatherMap API, and create an application that will allow users to enter a city name where they would like to query weather information for, then application will then construct the API call, and display to the user temperature information in Celcius. This API will require an API Key to work, and you will need to look around the documentation for instruction of how to acquire such key. Have fun coding!
+ */
