c2a1e1ca38d507d7cc9372bea26f273b73423dd9
laurielim <laurie.limsam@gmail.com>
2021-03-10 11:25:54 +0200

    get user input

--- basic-js/day-11/script.js
+++ basic-js/day-11/script.js
@@ -1,3 +1,26 @@
 /**
  * In this lesson you are expected to study the technical documentation of the the OpenWeatherMap API, and create an application that will allow users to enter a city name where they would like to query weather information for, then application will then construct the API call, and display to the user temperature information in Celcius. This API will require an API Key to work, and you will need to look around the documentation for instruction of how to acquire such key. Have fun coding!
  */
+
+(function () {
+  // Wait for user to press submit button
+  document.querySelector("button").addEventListener("click", function (event) {
+    // Stop page from refreshing
+    event.preventDefault();
+    executeScript();
+  });
+
+  function executeScript() {
+    // Get user input
+    let cityName = document.getElementById("city").value;
+    // If user did not input anything
+    if (!cityName) {
+      // Ask user to add input
+      alert("Please enter a city name");
+      // Exit function
+      return;
+    }
+
+    console.log(cityName);
+  }
+})();
